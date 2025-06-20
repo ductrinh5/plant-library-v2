026796278c8f8a8a6c2950d9de04b2d7823c59dd
ductrinh5 <ngocduc181103@gmail.com>
2025-06-21 02:17:45 +0700

    add url

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,8 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/plant-library-v2/',   // ← Add this line
   plugins: [react()],
 })
